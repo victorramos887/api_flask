9d614ff2493ed393d35a546c60f45c5f4e635b4d
victorramos887 <v.ramos58@hotmail.com>
2022-05-11 16:25:23 -0300

    ka

--- flasksql.py
+++ flasksql.py
@@ -5,3 +5,26 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URL'] = 'postgresql://postgres:postgres@192.168.1.199/gemini'
 db = SQLAlchemy(app)
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique = True)
+    email = db.Column(db.String(120), unique = True)
+
+    def __init__(self):
+        super().__init__()
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
+
+def index():
+    return '<h1 style=color:red>Hello Flask</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
