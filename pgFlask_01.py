e2d4ed24426f07d223c42608a0ebf10196a233c1
victorramos887 <v.ramos58@hotmail.com>
2022-05-11 14:54:42 -0300

    connect postgresql

--- pgFlask_01.py
+++ pgFlask_01.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import psycopg2 as pg
 import psycopg2.extras
+import json
+import pandas as pd
+from datetime import datetime
+
+
 
 #CRIANDO O APP FLASK
 app = Flask(__name__)
@@ -15,8 +20,28 @@ DB_PASS = 'postgres'
 
 conn = pg.connect(dbname = DB_NAME, user = DB_USER, password = DB_PASS, host = DB_HOST)
 
-@app.route('/')
-def index()
+@app.route('/<valor>')
+def index(valor):
+
+    s = """INSERT INTO main.datalogger VALUES (DEFAULT, '14-04-2022', '14-04-2022 10:45:00', 25, 15, 16, 25, 21)"""
+    leitura = {
+        "Id":valor[:3],
+        "Data": f'{valor[3:5]}/{valor[5:7]}/{valor[7:9]}',
+        "Hora": f'{valor[9:11]}:{valor[11:13]}',
+        "Temperatura": f'{valor[13:15]}',
+        "Sensor1": f'{valor[15:20]}',
+        "Sensor2":f'{valor[20:25]}',
+        "Sensor3":f'{valor[25:30]}',
+        "ControledePulsos":f'{valor[30:]}',
+        "DataHora": f"{valor[3:5]}/{valor[5:7]}/{valor[7:9]} {valor[9:11]}:{valor[11:13]}"
+               }
+
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    print(leitura['DataHora'])
+    cur.execute(s)
+    conn.commit()
+
+    return "Salvo"
 
 
 if __name__ == "__main__":
