ab90ad98cc739aa4e57e6ef75f11b56da9676d78
victorramos887 <v.ramos58@hotmail.com>
2022-09-09 08:08:36 -0300

    salvando trabalho de ontem

--- flask_07.py
+++ flask_07.py
@@ -3,11 +3,18 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['GET'])
 def index():
+    return "API NO AR"
+
+@app.route('/primeiraroute', methods = ['POST', 'GET'])
+def primeiraroute():
     #print(request.method, request.args)
     t1 = request.args.to_dict()
     t2 = dict(request.args)
+
+    print('-------------------- ANÁLISE DAS INFORMAÇÕES ------------------------')
+    print(t2)
     return json.dumps([t1['nome'], t2['idade'], t1['analise']])
 
 if __name__ == '__main__':
